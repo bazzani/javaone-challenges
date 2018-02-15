38fd10117646f25f6b261d8661f06829bc0dd951
Barry Evans <barryevans80@gmail.com>
2018-02-15 00:26:44 +0000

    Rearranging Lambda challenge after V1 DAFM presentation to help explain how a method reference is valid for the Revolver Functional Interface

--- src/main/java/com/javachallenges/javaone/LambdaChallenge.java
+++ src/main/java/com/javachallenges/javaone/LambdaChallenge.java
@@ -3,29 +3,30 @@ package com.javachallenges.javaone;
 @SuppressWarnings(value = "all")
 public class LambdaChallenge {
 	public static void main(String ... doYourBest) {
-		String vitoShoot = useRevolver(RevolverShoot::shoot);
-		String michaelShoot = useShotgun(
-		        () -> {return "shotgun";});
-		
-		System.out.println(vitoShoot + michaelShoot);
-	}
-	private static String useRevolver(Revolver revolver) {
-		return revolver.shoot();
+		String vitoShoot    = useRevolver(RevolverShoot::shoot);
+		String michaelShoot =  useShotgun(() -> {return "shotgun";});
+
+		System.out.println(String.join(" ", vitoShoot, michaelShoot));
 	}
 
-    static class RevolverShoot{
-        static String shoot(){ return "revolver"; }
-    }
+	static String useRevolver(Revolver revolver) {
+		return revolver.shoot();
+	}
 
 	static String useShotgun(Shotgun shotgun) {
 		return shotgun.shoot();
 	}
 
-    @FunctionalInterface
-    private interface Revolver {
-        String shoot();
-    }
-	public interface Shotgun {
+	@FunctionalInterface
+	private interface Revolver {
+		String shoot();
+	}
+
+	static class RevolverShoot {
+		static String shoot(){ return "revolver"; }
+	}
+
+	public static interface Shotgun {
 		String shoot();
 		default String reload() {return "reloading..."; }
 	}
