3c477e8e85b4a6d9208a34d1598199dbbc6937e9
Barry Evans <barry.evans@version1.com>
2018-05-05 01:10:20 +0100

    Add runBikeThreads script and display instance, last bike name, and start time of each ThreadChallenge in the console.

--- src/main/java/com/javachallenges/ljc/ThreadChallenge.java
+++ src/main/java/com/javachallenges/ljc/ThreadChallenge.java
@@ -1,9 +1,14 @@
 package com.javachallenges.ljc;
 
+import java.time.Instant;
+
 public class ThreadChallenge {
+    private static final String START_TIME = Instant.now().toString();
     private static int wolverineAdrenaline = 10;
+    private static String runNumber;
 
     public static void main(String... doYourBest) throws InterruptedException {
+        runNumber = doYourBest[0];
 
         new Motorcycle("first").start();
         new Motorcycle("second").start();
@@ -16,13 +21,23 @@ public class ThreadChallenge {
     }
 
     static class Motorcycle extends Thread {
-        public Motorcycle(String bikeName) { super(bikeName); }
+        public Motorcycle(String bikeName) {
+            super(runNumber + ":" + bikeName);
+        }
 
-        @Override public void run() {
+        @Override
+        public void run() {
             wolverineAdrenaline++;
             if (wolverineAdrenaline == 14) {
-                System.out.println(this.getName());
+                String startTimeSeconds = getStartTimeSeconds();
+                System.out.println(String.format(" > Last Bike   [%s] @ %s", this.getName(), startTimeSeconds));
             }
+        }
+
+        private String getStartTimeSeconds() {
+            int beginIndex = START_TIME.length() - 10;
+            int endIndex = START_TIME.length() - 1;
+            return START_TIME.substring(beginIndex, endIndex);
         }
     }
 }
